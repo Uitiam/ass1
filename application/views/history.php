77bda67bcc2f1db23eab1b07d8357c99ae02be1b
John Agapeyev <jagapeyev@gmail.com>
2017-02-09 16:29:55 -0800

    Updated view to remove template and use bootstrap
    Signed-off-by: John Agapeyev <jagapeyev@gmail.com>

--- application/views/history.php
+++ application/views/history.php
@@ -1,30 +1,7 @@
-<?php
-if (!defined('APPPATH'))
-	exit('No direct script access allowed');
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>{pagetitle}</title>
-        <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
-        <link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
-    </head>
-    <body>
-        <div class="container">
-            <div id="content">
-                <h1>{pagetitle}</h1>
-                {history}
-                <div class="row">
-                    <div class="span4"><p>{type}</p></div>
-                    <div class="span4"><p>{data}</p></div>
-                    <div class="span4"><p>{datetime}</p></div>
-                </div>
-                {/history}
-            </div>
-        </div>
-        <script src="/assets/js/jquery-1.11.1.min.js"></script>
-        <script src="/assets/js/bootstrap.min.js"></script>
-    </body>
-</html>
+{history}
+<div class="row">
+    <div class="col-xs-4"><p>{type}</p></div>
+    <div class="col-xs-4"><p>{data}</p></div>
+    <div class="col-xs-4"><p>{datetime}</p></div>
+</div>
+{/history}
