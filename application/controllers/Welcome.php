b4b88d768b4dce68e151b0f916d713d59f01139a
Michael Goll <mike_g97@hotmail.com>
2017-02-09 21:13:28 -0800

    Gutted original, added template and changed default rendered view for "Welcome.php" controller
    Signed-off-by: Michael Goll <mike_g97@hotmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -16,16 +16,7 @@ class Welcome extends Application
 	public function index()
 	{
 		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
-
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-		$authors = array ();
-		foreach ($source as $record)
-		{
-			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
-		}
-		$this->data['authors'] = $authors;
+		$this->data['pagebody'] = 'landing';
 
 		$this->render();
 	}
