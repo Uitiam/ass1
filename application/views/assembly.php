5739483ba3df594e530a9622af853cdb5d10f36e
Uitiam <maitiumorton@gmail.com>
2017-02-11 10:18:04 -0800

    FIxed the assembly page footer bug where the button overlapped the footer
    Signed-off-by: Uitiam <maitiumorton@gmail.com>

--- application/views/assembly.php
+++ application/views/assembly.php
@@ -1,66 +1,66 @@
-<div class="col-xs-6 sidebar-outer">
-    <div class="row">
-        {heads}
-            <div class="col-xs-3" style="margin-bottom: 20px;">
-                <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
-                <div align="center">
-                    <button id="{title}" type="button" class="btn btn-success btn-sm">Use</button>
-                    <button id="{title}" type="button" class="btn btn-danger btn-sm">Scrap</button>
+<div class="row">
+    <div class="col-xs-6 sidebar-outer">
+        <div class="row">
+            {heads}
+                <div class="col-xs-3" style="margin-bottom: 20px;">
+                    <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
+                    <div align="center">
+                        <button id="{title}" type="button" class="btn btn-success btn-sm">Use</button>
+                        <button id="{title}" type="button" class="btn btn-danger btn-sm">Scrap</button>
+                    </div>
                 </div>
-            </div>
-        {/heads}
-    </div>
-    <div class="row">
-        {torso}
-            <div class="col-xs-3" style="margin-bottom: 20px;">
-                <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
-                <div align="center">
-                    <button id="{title}" type="button" class="btn btn-success btn-sm">Use</button>
-                    <button id="{title}" type="button" class="btn btn-danger btn-sm">Scrap</button>
+            {/heads}
+        </div>
+        <div class="row">
+            {torso}
+                <div class="col-xs-3" style="margin-bottom: 20px;">
+                    <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
+                    <div align="center">
+                        <button id="{title}" type="button" class="btn btn-success btn-sm">Use</button>
+                        <button id="{title}" type="button" class="btn btn-danger btn-sm">Scrap</button>
+                    </div>
                 </div>
-            </div>
-        {/torso}
-    </div>
-    <div class="row">
-        {legs}
-            <div class="col-xs-3" style="margin-bottom: 20px;" >
-                <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
-                <div align="center">
-                    <button id="{title}" type="button" class="btn btn-success btn-sm">Use</button>
-                    <button id="{title}" type="button" class="btn btn-danger btn-sm">Scrap</button>
+            {/torso}
+        </div>
+        <div class="row">
+            {legs}
+                <div class="col-xs-3" style="margin-bottom: 20px;" >
+                    <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
+                    <div align="center">
+                        <button id="{title}" type="button" class="btn btn-success btn-sm">Use</button>
+                        <button id="{title}" type="button" class="btn btn-danger btn-sm">Scrap</button>
+                    </div>
                 </div>
-            </div>
-        {/legs}
+            {/legs}
+        </div>
     </div>
-</div>
 
 
-<!-- Recommended Robot-->
-<div class="col-xs-12 col-md-5 col-lg-5">
-    <div class="container col-xs-12 col-md-12 col-lg-12">
-        {robotHead}
-        <div class="thumbnail">
-            <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
-            <div class="caption">
+    <!-- Recommended Robot-->
+    <div class="container-fluid col-xs-12 col-md-5 col-lg-5">
+            {robotHead}
+            <div class="thumbnail">
+                <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
+                <div class="caption">
+                </div>
             </div>
-        </div>
-        {/robotHead}
-        {robotTorso}
-        <div class="thumbnail">
-            <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
-            <div class="caption">
+            {/robotHead}
+            {robotTorso}
+            <div class="thumbnail">
+                <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
+                <div class="caption">
+                </div>
             </div>
-        </div>
-        {/robotTorso}
-        {robotLegs}
-        <div class="thumbnail">
-            <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
-            <div class="caption">
+            {/robotTorso}
+            {robotLegs}
+            <div class="thumbnail">
+                <img class="img-responsive img-rounded" src="/assets/img/{src}" title="{title}"/>
+                <div class="caption">
+                </div>
             </div>
-        </div>
-        {/robotLegs}
-        <div align="right">
-            <button id="{title}" type="button" class="btn btn-success btn-md">Build Robot</button>
-        </div>
+            {/robotLegs}
+            <div align="right">
+                <button id="{title}" type="button" class="btn btn-success btn-md">Build Robot</button>
+            </div>
     </div>
 </div>
