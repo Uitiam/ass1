e38e17c6a6df18dcc8d76900a50afd5806dd1478
Isaac <isaacmorneau@gmail.com>
2017-02-11 10:40:18 -0800

    Added comments and summation methods to models
    Signed-off-by: Isaac <isaacmorneau@gmail.com>

--- application/models/History.php
+++ application/models/History.php
@@ -1,6 +1,8 @@
 <?php
 
 class History extends CI_Model {
+
+    //mock data array
 	var $data = array(
 		array('id' => '1', 'type' => 'Purchase', 'data' => 'Part abcdef bought for $13.99', 'cost' =>13.99 , 'sold' =>0 , 'datetime' => '14:49:26 09-02-2017'),
 		array('id' => '2', 'type' => 'Assembly', 'data' => 'Bot A2 built', 'cost' =>12, 'sold' =>30, 'datetime' => '14:49:26 09-02-2017'),
@@ -13,7 +15,7 @@ class History extends CI_Model {
 		parent::__construct();
 	}
 
-	// retrieve a single quote
+    //find a record with the id of $which
 	public function get($which)
 	{
 		// iterate over the data until we find the one we want
@@ -23,29 +25,27 @@ class History extends CI_Model {
 		return null;
 	}
 
-	// retrieve all of the quotes
+    //return all records
 	public function all()
 	{
 		return $this->data;
 	}
 
+    //calculate the total cost so far
     public function cost() {
         $sum = 0;
-        
 		$data = $this->data();
-		foreach ($data as $trans)
-		{
+		foreach ($data as $trans) {
             $sum += $trans['cost'];
         }
         return $sum;
     }
 
+    //calculate the total sales so far
     public function sales() {
         $sum = 0;
-        
 		$data = $this->data();
-		foreach ($data as $trans)
-		{
+		foreach ($data as $trans) {
             $sum += $trans['sold'];
         }
         return $sum;
