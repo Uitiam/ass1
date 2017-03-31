61713091c3587197a0bf0e569d094f88fca77dc4
John Agapeyev <jagapeyev@gmail.com>
2017-03-30 22:06:32 -0700

    Replaced tabs with 4 spaces
    Signed-off-by: John Agapeyev <jagapeyev@gmail.com>

--- application/controllers/Parts.php
+++ application/controllers/Parts.php
@@ -5,32 +5,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Parts extends Application
 {
 
-	function __construct()
-	{
-		parent::__construct();
+    function __construct()
+    {
+        parent::__construct();
         session_start();
-	}
+    }
 
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'parts';
+    /**
+     * Homepage for our app
+     */
+    public function index()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'parts';
 
-		// build the list of authors, to pass on to our view
-		$source = $this->part->all();
-		$parts = array();
-		foreach ($source as $record)
-		{
+        // build the list of authors, to pass on to our view
+        $source = $this->part->all();
+        $parts = array();
+        foreach ($source as $record)
+        {
             $parts[] = array ('src' => $record['src'], 'title' => $record['title'], 'UID'=> $record['UID'],
             'CA'=> $record['CA'], 'PlantCode'=>$record['PlantCode'], 'datetime'=>$record['datetime']);
-		}
-		$this->data['parts'] = $parts;
+        }
+        $this->data['parts'] = $parts;
 
-		$this->render();
-	}
+        $this->render();
+    }
 
     private function getKey() {
         $response = '';
