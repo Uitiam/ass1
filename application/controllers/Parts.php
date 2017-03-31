193688c8f8b115f564b1efb4d65877f7711e94a2
John Agapeyev <jagapeyev@gmail.com>
2017-03-30 21:23:31 -0700

    Modified parts to make requests for buying and building parts
    Signed-off-by: John Agapeyev <jagapeyev@gmail.com>

--- application/controllers/Parts.php
+++ application/controllers/Parts.php
@@ -8,6 +8,7 @@ class Parts extends Application
 	function __construct()
 	{
 		parent::__construct();
+        $this->load->library('session');
 	}
 
 	/**
@@ -30,5 +31,58 @@ class Parts extends Application
 
 		$this->render();
 	}
+
+    private function getKey() {
+        $key = $_SESSION['key'];
+        $url = "https://umbrella.jlparry.com/info/whoami?key=" . $key;
+        $response = $this->makeRequest($url);
+        if (strcmp($response, "zucchini") != 0) {
+            //Get password here
+            //$pass = file_get_contents("../data/password.txt");
+            $url = "https://umbrella.jlparry.com/work/registerme/zucchini/" . $pass;
+            $key = $this->makeRequest($url);
+            $_SESSION['key'] = $key;
+        }
+        return $key;
+    }
+
+    private function makeRequest($url) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+
+    public function build() {
+        $url = "https://umbrella.jlparry.com/work/mybuilds?key=" . $this->getKey();
+        $response = $this->makeRequest($url);
+        echo $response;
+        $response = json_decode($response);
+
+        foreach ($response as $part) {
+            //id
+            //model
+            //piece
+            //stamp
+        }
+    }
+
+    public function buy() {
+        $url = "https://umbrella.jlparry.com/work/buybox?key=" . $this->getKey();
+        $response = $this->makeRequest($url);
+        echo $response;
+        $response = json_decode($response);
+
+        foreach ($response as $part) {
+            //id
+            //model
+            //piece
+            //stamp
+        }
+    }
 
 }
--- application/views/parts.php
+++ application/views/parts.php
@@ -1,3 +1,4 @@
+<script src="assets/js/parts.js"></script>
 <div>
     <div class="page-header">
     <h1>Parts <small>Full catalog</small></h1>
@@ -42,7 +43,7 @@
     </div>
     {/parts}
     <div align="center">
-        <button id="{title}" type="button" class="btn btn-success btn-sm" style="width: 200px">Build More Parts</button>
-        <button id="{title}" type="button" class="btn btn-danger btn-sm" style="width: 200px">Buy More Parts</button>
+        <a href="/parts/build"><button id="{title}" type="button" class="btn btn-success btn-sm" style="width: 200px">Build More Parts</button></a>
+        <a href="/parts/buy"><button id="{title}" type="button" class="btn btn-danger btn-sm" style="width: 200px">Buy More Parts</button></a>
     </div>
 </div>
