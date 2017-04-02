77789ff4e0730a399ee171956d313084aeb62192
Uitiam <maitiumorton@gmail.com>
2017-04-02 01:11:40 -0700

    Inserting New Robots into Robot Table
    Signed-off-by: Uitiam <maitiumorton@gmail.com>

--- application/models/Robot.php
+++ application/models/Robot.php
@@ -34,48 +34,43 @@ class Robot extends CI_Model {
     */
     public function addRobot($part1, $part2, $part3){
 
-
-
-        /*$eV = $this->db->escape($part1);
+        $eV = $this->db->escape($part1);
         $sql = "select * from head where id=1";
-        $h[] = $this->db->query($sql)->result_array();
+        $h = $this->db->query($sql)->result_array()[0]['model'];
 
         $eV = $this->db->escape($part2);
-        $sql[] = "select * from torso where id=1";
-        $t = $this->db->query($sql)->result_array();
+        $sql = "select * from torso where id=1";
+        $t = $this->db->query($sql)->result_array()[0]['model'];
 
         $eV = $this->db->escape($part3);
-        $sql[] = "select * from legs where id=1";
-        $l = $this->db->query($sql)->result_array();
+        $sql = "select * from legs where id=1";
+        $l = $this->db->query($sql)->result_array()[0]['model'];
 
-        /*$hModel = partModel($h);
-        $tModel = partModel($t);
-        $lModel = partModel($l);
+        $hModel = $this->partModel($h);
+        $tModel = $this->partModel($t);
+        $lModel = $this->partModel($l);
         $model = "!";
-        $bBot = "l";
-        $cBot = "c";
 
-        /*if(strcmp($hmodel, $tModel) == 0 && strcmp($hmodel, $lModel) == 0){
+        if(strcmp($hModel, $tModel) == 0 && strcmp($hModel, $lModel) == 0){
             if(strcmp($h, $t) == 0 && strcmp($h, $l) == 0){
-                $model = strtoupper($hmodel);
+                $model = strtoupper($hModel);
             } else {
-                $midel = $hmodel;
+                $model = $hModel;
             }
-        }*/
+        }
 
         $p1 = $this->db->escape($part1);
         $p2 = $this->db->escape($part2);
         $p3 = $this->db->escape($part3);
-        $p4 = 2;
 
-        $this->db->query("insert into Robot(headId, torsoId, legsId, used)
-        VALUES ($p1, $p2, $p3, 'f')");
+        $this->db->query("insert into Robot(headId, torsoId, legsId, used, model)
+        VALUES ($p1, $p2, $p3, 'f', '$model')");
     }
 
     public function partModel($model){
         if(strcmp("$model", "l") > 0){
             return 'h';
-        } else if (strcmp("$model", "l") > v){
+        } else if (strcmp("$model", "l") > 0){
             return 'b';
         } else {
             return 'c';
